4aef0dd92e302378e6e51b0ad5d8ab406aa6f2ba
Kalagiri Rakesh <rakesh.kalagiri@recykal.com>
2026-05-19 16:50:33 +0530

    Added log

--- FE_DebtTracker/src/components/DashboardCard.jsx
+++ FE_DebtTracker/src/components/DashboardCard.jsx
@@ -1,31 +1,33 @@
 const DashboardCard = ({ title, amount, color, icon }) => {
+  console.log("DashboardCard");
   return (
-    <div style={{
-      height: "130px",
-      background: color,
-      color: "white",
-      borderRadius: "14px",
-      padding: "20px",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between",
-      boxShadow: "0 6px 15px rgba(0,0,0,0.15)"
-    }}>
-      
+    <div
+      style={{
+        height: "130px",
+        background: color,
+        color: "white",
+        borderRadius: "14px",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        boxShadow: "0 6px 15px rgba(0,0,0,0.15)",
+      }}
+    >
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h3 style={{ 
-          fontSize: "16px", 
-          fontWeight: "600", 
-          textTransform: "uppercase" 
-        }}>
+        <h3
+          style={{
+            fontSize: "16px",
+            fontWeight: "600",
+            textTransform: "uppercase",
+          }}
+        >
           {title}
         </h3>
         {icon}
       </div>
 
-      <h1 style={{ fontSize: "26px", fontWeight: "bold" }}>
-        {amount}
-      </h1>
+      <h1 style={{ fontSize: "26px", fontWeight: "bold" }}>{amount}</h1>
     </div>
   );
 };
